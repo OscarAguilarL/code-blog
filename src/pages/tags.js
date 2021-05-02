891af05deec960f71a9b3922b2d5cc676db6c9f4
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-05-02 14:28:20 -0500

    Imported some style files to the proyect

---
+++ src/pages/tags.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const TagsPage = () => (
+  <Layout>
+    <Seo title="Tags" />
+    <h1>Tags page</h1>
+  </Layout>
+)
+
+export default TagsPage
