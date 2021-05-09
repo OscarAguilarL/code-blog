08c3e87cada9dd50adc95a0a8f5fc065b06dcb4c
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-05-09 11:17:43 -0500

    Created paginated pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,6 +23,7 @@ exports.createPages = ({ actions, graphql }) => {
     singlePost: path.resolve('src/templates/single-post.js'),
     tagsPage: path.resolve('src/templates/tags-page.js'),
     tagPosts: path.resolve('src/templates/tag-posts.js'),
+    postList: path.resolve('src/templates/post-list.js')
   }
 
   return graphql(`
@@ -95,6 +96,26 @@ exports.createPages = ({ actions, graphql }) => {
         component: templates.tagPosts,
         context: {
           tag,
+        }
+      })
+    })
+
+    const postPerPage = 2;
+    const numberOfPages = Math.ceil(posts.length / postPerPage);
+
+    Array.from({ length: numberOfPages }).forEach((_, index) => {
+      const isFirstPage = index === 0;
+      const currentPage = index + 1;
+
+      if (isFirstPage) return
+
+      createPage({
+        path: `/page/${currentPage}`,
+        component: templates.postList,
+        context: {
+          limit: postPerPage,
+          skip: index * postPerPage,
+          currentPage
         }
       })
     })
--- src/pages/index.js
+++ src/pages/index.js
@@ -37,6 +37,7 @@ const indexQuery = graphql`
   query {
     allMarkdownRemark (
       sort: { fields: [frontmatter___date], order: DESC }
+      limit: 2
       ){
       edges{
         node{
